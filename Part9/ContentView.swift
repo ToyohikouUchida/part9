36c7e3acdf1b0cf1ab99740827f29b52bfa3965f
ToyohikoUchida <tyhkucd@gmail.com>
2024-08-27 19:50:41 +0900

    Alert & Dialog final

--- Part9/ContentView.swift
+++ Part9/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isShowAlert = false
+    @State var isShowDialog = false
     
     var body: some View {
         VStack {
@@ -16,15 +17,46 @@ struct ContentView: View {
                 isShowAlert = true
             }
             .padding()
+            Button("ダイアログ"){
+                isShowDialog = true
+            }
+            .padding()
         }
         .padding()
-        .alert("本当に削除しますか",isPresented: $isShowAlert) {
-            Button("OK") {
+        .alert("本当に削除しますか", isPresented: $isShowAlert)
+            {
+            Button("削除する", role: .destructive) {
                 
             }
+            Button("キャンセル", role: .cancel) {
+                
+            }
+            
         } message: {
             Text("一度削除したら元に戻どすことは出来ません")
         }
+        
+        .confirmationDialog("タイトル", isPresented: $isShowDialog, titleVisibility: .visible) {
+            Button("選択肢1"){
+                
+            }
+            Button("選択肢2"){
+                
+            }
+            Button("選択肢3"){
+                
+            }
+            Button("選択肢4"){
+                
+            }
+            
+            Button("キャンセル", role: .cancel){
+                
+            }
+        } message: {
+            Text("タイトル")
+        }
+        
     }
 }
 

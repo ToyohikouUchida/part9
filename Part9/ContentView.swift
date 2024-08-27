2e0ae78acca7346f5b4a5b125f2373459de6ff9c
ToyohikoUchida <tyhkucd@gmail.com>
2024-08-27 18:04:35 +0900

    first commit after add

---
+++ Part9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Part9
+//
+//  Created by 内田豊彦 on 2024/08/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Part9/Part9App.swift
@@ -0,0 +1,17 @@
+//
+//  Part9App.swift
+//  Part9
+//
+//  Created by 内田豊彦 on 2024/08/27.
+//
+
+import SwiftUI
+
+@main
+struct Part9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
